86ee4ccaa7a76579308b9b24eaf3130819714940
Sid <chinnusreddy@gmail.com>
2024-03-30 19:23:59 -0400

    folder structure created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Sid Gurram',
+    author_email= 'siddardha.gurram@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
